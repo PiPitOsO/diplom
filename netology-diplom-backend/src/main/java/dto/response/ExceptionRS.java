e95bb570b93a16e583eb5669e62aed392c50003e
Vladislav Khomchenkov <vladdiablo1@mail.ru>
2022-06-17 15:12:20 +0300

    init

---
+++ netology-diplom-backend/src/main/java/dto/response/ExceptionRS.java
@@ -0,0 +1,12 @@
+package dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ExceptionRS {
+
+    private String message;
+    private Integer id;
+}
---
+++ netology-diplom-backend/src/main/java/dto/response/FileRS.java
@@ -0,0 +1,12 @@
+package dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class FileRS {
+
+    private String filename;
+    private Long size;
+}
